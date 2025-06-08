490a638d9013c24dd50a7efa330953a0ccf9622f
PFM37 <pradhananjana33@gmail.com>
2025-06-08 19:48:40 +0530

    .

--- test.js
+++
@@ -1,6 +0,0 @@
-const flags = args.filter(arg => arg.startsWith("-"));
-const params = args.filter(arg => !arg.startsWith("-"));
-
-if (flags.includes("--force") || flags.includes("-f")) {
-  console.log("Force flag detected!");
-}
